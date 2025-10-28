aeed60a3623ac0ec765eee8ce50c9b8915d60947
aschne-dev <a.schneberger+dev@gmail.com>
2025-10-28 05:58:21 +0100

    style notFoudPage

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,11 +1,14 @@
+import { Link } from "react-router";
 import Header from "../components/Header";
 
 export default function NotFound() {
   return (
     <>
       <Header />
-      <div>
-        <p className="not-found">Not found</p>
+      <div className="not-found">
+        <h1>Page not found</h1>
+        <p>We can't seem to find the page you're looking for.</p>
+        <Link to="/">Back to homepage</Link>
       </div>
     </>
   );
